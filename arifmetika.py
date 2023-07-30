e4234ba22c6ef53c375877a0266599cda9b449a3
Anvar <anvar.sharipov.1986@gmail.com>
2023-07-30 18:27:16 +0300

    commit s novoi vetki, vetka_1

--- arifmetika.py
+++ arifmetika.py
@@ -1,1 +1,44 @@
+# print('Python', end='\n\n\n')
+# print('a', 'b', 'c', sep='', end='')
 
+
+# print('a', 'b', 'c', sep='*')
+# print('d', 'e', 'f', sep='**', end='')
+# print('g', 'h', 'i', sep='+', end='%')
+# print('j', 'k', 'l', sep='-', end='\n')
+# print('m', 'n', 'o', sep='/', end='!')
+# print('p', 'q', 'r', sep='1', end='%')
+# print('s', 't', 'u', sep='&', end='\n')
+# print('v', 'w', 'x', sep='%')
+# print('y', 'z', sep='/', end='!')
+
+# print("I", "like", "Python", sep='***')
+
+
+
+# a, b, c, d = input(), input(), input(), input()
+# print(b, a, c, a, d, sep='')
+
+
+# name = input()
+# print("Привет,", name, "!", sep='')
+
+
+# num = int(input())
+#
+# print(num)
+# print(num + 1)
+# print(num + 2)
+
+
+# num_1, num_2, num_3 = int(input()), int(input()), int(input())
+#
+# print(num_1+num_2+num_3)
+
+
+# dlina_rebra_kuba = int(input())
+# v = dlina_rebra_kuba ** 3
+# s = 6 * dlina_rebra_kuba ** 2
+#
+# print("Объем = ", v)
+# print("Площадь подной поверхности  = ", s)
---
+++ vetka_praktik_1.py
@@ -0,0 +1,1 @@
+'''Sozdal novuyu vetku dlya praktiki'''
