16015630c767bdc666874abfb0fdfa9f83bb1d72
Anvar <anvar.sharipov.1986@gmail.com>
2023-08-14 17:25:09 +0300

    nachal modul chislovie tipi

---
+++ chislovyie_tipyi.py
@@ -0,0 +1,26 @@
+''' novyi modul chislovyie tipyi dannyikh '''
+
+# ''' ploshad treugolnika'''
+
+# n, r = float(input()), float(input())
+#
+# s = (1/2) * n * r
+# print(s)
+
+
+# ''' skorost babulek '''
+#
+# s, v1, v2 = float(input()), float(input()), float(input())
+#
+# t = s / (v1 + v2)
+# print(t)
+
+
+# ''' obratnoe chislo '''
+#
+# n = float(input())
+# if n == 0:
+#     print("Обратного числа не существует")
+# else:
+#     s = (n ** -1 )
+#     print(s)
