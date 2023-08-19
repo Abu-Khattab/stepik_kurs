cd60a95ae2198485ee7273169d2e3e5eaed4536c
Anvar <anvar.sharipov.1986@gmail.com>
2023-08-19 14:58:08 +0300

    podmodul modulya for

---
+++ cikl_for.py
@@ -0,0 +1,58 @@
+''' modul pro cikli '''
+
+# s = 'Python is awesome!'
+# for i in range(10):
+#     print(s)
+
+
+# s = input()
+# s1 = int(input())
+# for i in range(s1):
+#     print(s)
+
+
+
+# for i in range(6):
+#     print('A' * 3)
+# for i in range(5):
+#     print('B' * 4)
+# print('E')
+# for i in range(9):
+#     print('T' * 5)
+# print('G')
+
+
+# n = int(input())
+# for i in range(n):
+#     print('*' * 19)
+
+
+# n = input()
+# for i in range(10):
+#     print(i, n)
+
+
+
+# ''' Квадрат числа '''
+
+# n = int(input())
+# for i in range(n + 1):
+#     print("Квадрат числа", i, "равен", i ** 2)
+
+
+
+# ''' Звездный треугольник '''
+
+# n = int(input())
+# for i in range(n, 0, -1):
+#     i = i * '*'
+#     print(i)
+
+
+
+# ''' Популяция '''
+#
+# m, p, n = float(input()), float(input()), int(input())
+# for i in range(n):
+#     s = m * (p / 100 + 1) ** i
+#     print(i + 1, s)
