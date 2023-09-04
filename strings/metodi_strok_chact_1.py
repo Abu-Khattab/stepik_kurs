b278b36514899337aa7a9d3c9b391d22d534dbd2
Anvar <anvar.sharipov.1986@gmail.com>
2023-09-04 16:38:05 +0300

    закончил подмодуль методы строк часть 1

---
+++ strings/metodi_strok_chact_1.py
@@ -0,0 +1,34 @@
+# ''' Заглавные буквы '''
+#
+#
+# s = input()
+# if s == s.title():
+#     print("YES")
+# else:
+#     print("NO")
+
+
+# ''' sWAP cASE '''
+#
+# s = input()
+# print(s.swapcase())
+
+
+# ''' Хороший оттенок '''
+#
+# s = input().lower()
+# if 'хорош' in s:
+#     print("YES")
+# else:
+#     print("NO")
+
+
+# ''' Нижний регистр '''
+#
+# s = input()
+# low = 'abcdefghijklmnopqrstuvwxyz'
+# counter = 0
+# for i in s:
+#     if i in low:
+#         counter += 1
+# print(counter)
