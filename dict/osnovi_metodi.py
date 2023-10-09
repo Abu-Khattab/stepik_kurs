55a708568777841175851c372c2a37b46963787a
Anvar <anvar.sharipov.1986@gmail.com>
2023-10-09 15:21:48 +0300

    закончил подмодуль dict{}

---
+++ dict/osnovi_metodi.py
@@ -0,0 +1,57 @@
+# info = dict(name='Timur', age=28, job='Teacher')
+# print(info)
+
+
+# info_tuple = (['name', 'Timur'], ['age', 28], ['job', 'Teacher'])  # кортеж списков
+# print(dict(info_tuple))  # создаем словарь на основе кортежа списков
+
+
+# dict1 = dict.fromkeys(['name', 'age', 'job'], 'Missed information')
+# print(dict1)
+
+
+# dict1 = {}
+# dict2 = dict()
+#
+#
+# print(dict1)
+# print(dict2)
+# print(type(dict1))
+# print(type(dict2))
+
+
+
+
+# languages = {'Python': 'Гвидо ван Россум',
+#              'C#': 'Андерс Хейлсберг',
+#              'Java': 'Джеймс Гослинг'}
+#
+# info = dict(name='Timur', age=28, level='sinor', language='python', job='Teacher')
+#
+# print(languages)
+# print(info)
+
+
+
+
+# info = {'name': 'Timur',
+#         'age': 28,
+#         'job': 'Teacher',
+#         'city': 'Moscow',
+#         'email': 'timyr-guev@yandex.ru'}
+#
+# print(info['name'])
+# print(info['email'])
+# print(info['city'])
+
+
+# ''' словарь на основании двух списков (кортежей) '''
+
+# keys = ['name', 'age', 'job', 'city', 'email']
+# values = ['Timur', 28, 'Teacher', 'Moscow', 'tima.email.ru']
+#
+# info = dict(zip(keys, values))
+#
+# print(info)
+
+
